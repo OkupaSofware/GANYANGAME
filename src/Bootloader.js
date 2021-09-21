f31d84de55f84b405360a77337a23cf9674a8573
AEspinosaDev <79087129+AEspinosaDev@users.noreply.github.com>
2021-09-21 20:20:01 +0200

    first commit

---
+++ src/Bootloader.js
@@ -0,0 +1,15 @@
+class Bootloader extends Phaser.Scene {
+    constructor() {
+        super('Bootloader'); 
+    }
+
+    preload() {
+        
+        
+    }
+
+    create() {
+        //this.add.text(100, 100, "hola", {color: '#000'})
+    }
+}
+export default Bootloader;
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Bootloader from './Bootloader.js';
+
+const config = {
+    title: "GAN-YAN",
+    version: "0.0.1",
+    type: Phaser.AUTO,
+    scale: {
+        parent: "phaser_container",
+        width: 1280 ,
+        height: 720,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    //backgroundColor: "#ffffff",
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        "arcade": {
+            gravity: {
+                y: 500
+            }
+        }
+    },
+    scene: [
+        Bootloader
+    ]
+};
+
+new Phaser.Game(config);
