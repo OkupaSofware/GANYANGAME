b32f5f3643cc161cb122b91a235366fee6f8c614
Javinigadela <90959761+Javinigadela@users.noreply.github.com>
2021-09-22 21:14:31 +0200

    carga más elementos y añade funcion load.on

--- src/Bootloader.js
+++ src/Bootloader.js
@@ -4,8 +4,21 @@ class Bootloader extends Phaser.Scene {
     }
 
     preload() {
+        this.load.on("complete", () =>{
+            this.scene.start("MainMenu");
+        });
+
+        // MAIN MENU SPRITES
         this.load.spritesheet("test_buttons", "assets/buttons_test.png",{ frameWidth: 236, frameHeight: 65 });
-        this.scene.start("MainMenu");
+        //this.scene.start("MainMenu");
+
+        // SCENE PLAY SPRITES
+        // Environment sprites
+        this.load.image("background", "./assets/background.png");
+        this.load.image("platform", "./assets/platform.png");
+        
+        // Player sprite
+        this.load.image("player", "./assets/player.png");
     }
 
     create() {
