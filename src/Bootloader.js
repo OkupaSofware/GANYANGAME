b487f856c0d4fbb0914b73197f8b636a5a9c3c13
Alvaro <alvaroolavD2000@gmail.com>
2021-09-23 21:17:05 +0200

    animaciones

--- src/Bootloader.js
+++ src/Bootloader.js
@@ -20,12 +20,33 @@ class Bootloader extends Phaser.Scene {
         this.load.image("cloud", "./assets/cloud.png");
         
         // Player sprite
-        this.load.image("player", "./assets/player.png");
+        this.load.path = './assets/';
+
+        //idle
+        this.load.json('idle_anim','idle_anim.json')
+        this.load.atlas('idle', 'idle.png', 'idle_atlas.json')
+        //correr
+        this.load.json('run_anim','run_anim.json')
+        this.load.atlas('run', 'run.png', 'run_atlas.json')
+        //morir
+        this.load.json('die_anim','die_anim.json')
+        this.load.atlas('die', 'die.png', 'die_atlas.json')
+        //saltar
+        this.load.json('jump_anim','jump_anim.json')
+        this.load.atlas('jump', 'jump.png', 'jump_atlas.json')
     }
 
     create() {
         //this.add.text(100, 100, "hola", {color: '#000'})
-        
+        this.player_anim_idle = this.cache.json.get('idle_anim')
+        this.player_anim_run = this.cache.json.get('run_anim')
+        this.player_anim_die = this.cache.json.get('die_anim')
+        this.player_anim_jump = this.cache.json.get('jump_anim')
+
+        this.anims.fromJSON(this.player_anim_idle)
+        this.anims.fromJSON(this.player_anim_run)
+        this.anims.fromJSON(this.player_anim_die)
+        this.anims.fromJSON(this.player_anim_jump)
     }
 }
 export default Bootloader;
--- src/scenes/ScenePlay.js
+++ src/scenes/ScenePlay.js
@@ -22,30 +22,65 @@ class ScenePlay extends Phaser.Scene {
         });   
         options_button.setScale(0.65,0.65);
         options_button.on('pointerdown',this.optionsOnPressed,this)
+
+        //________________________________________________________
+
+        // Add platforms
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, "platform").setScale(3, 1);
+        this.platforms.create(700, 568, "platform").setScale(2, 2);
+        //this.platforms.setVelocity(0, 0);
+
+        this.p1 = this.physics.add.image(600, 400, "platform");
+        //p1.setVelocity(0, 20);
         
-    // Add platforms
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(400, 568, "platform").setScale(3, 1);
-    this.platforms.create(700, 568, "platform").setScale(2, 2);
-    //this.platforms.setVelocity(0, 0);
+        //________________________________________________________
 
-    this.p1 = this.physics.add.image(600, 400, "platform");
-    //p1.setVelocity(0, 20);
-    
-    this.player = new Player(this, 50, 50, "player");
+        //PLAYER
+        this.player = new Player(this, 50, 50, "idle");
 
-    this.input.on('pointerdown', function (pointer) {
-        this.player.shootAt(this.player.x, this.player.y, pointer.x, pointer.y);
-    }, this);
+        //animations
+        
 
-
-    }
-    optionsOnPressed(){
-        this.scene.start("MainMenu")
+        //controls
+        this.input.on('pointerdown', function (pointer) {
+            this.player.shootAt(this.player.x, this.player.y, pointer.x, pointer.y);
+        }, this);
+        
+        this.cQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q)
+        this.cW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        this.cE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E)
+        this.cD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        this.cA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
     }
 
     update(){
-        this.player.basicMovement();
+        this.player.body.setVelocityX(0)
+        
+        if(this.cD.isDown){
+            this.player.flipX = true;
+            this.player.body.setVelocityX(450)
+        }
+        if(this.cA.isDown){
+            this.player.flipX = false;
+            this.player.body.setVelocityX(-450)
+        }
+        if(this.cW.isDown && this.player.body.onFloor()){
+            this.player.body.setVelocityY(-400)
+        }
+        if((this.cA.isDown || this.cD.isDown) && this.player.body.onFloor()){
+            this.player.anims.play('run',true)
+        }
+        if(!this.player.body.onFloor()){
+            this.player.anims.play('jump',true)
+        }
+        if(!(this.cA.isDown || this.cD.isDown) && this.player.body.onFloor()){
+            this.player.anims.play('idle',true)
+        }
+    }
+
+    optionsOnPressed(){
+        this.scene.start("MainMenu")
     }
 }
 
