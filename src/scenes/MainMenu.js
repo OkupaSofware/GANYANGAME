9748ff3f2cd27f5d6d6450ba13642e5d0232a4b6
= <=>
2021-10-02 14:49:14 +0200

    fondo animado

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -16,12 +16,17 @@ class MainMenu extends Phaser.Scene {
        this.cloud1 = this.add.image(-200,0,"cloud");
        this.cloud1.setScale(0.2,0.2)
        this.titleText=this.add.text(640,80, "WELCOME TO",{font: 'bold 32px Arial', fontSize: "36px"}).setOrigin(0.5,0.5)
-       this.title = this.add.sprite(640,200, "title_anim")
-       this.title.setScale(0.2,0.2)
+       this.title = this.add.sprite(640,165, "title_anim")
+       this.title.setScale(0.15,0.15)
        this.anims.create({key: "colorful_title",frames: this.anims.generateFrameNumbers("title_anim", {frames: [0,1,2,3,4]}), repeat: -1, frameRate: 8})
        this.title.anims.play("colorful_title")
-
-
+       this.rights = this.add.text (640,700, "Okupa Software. All rights reserved. 2021.",{font: 'bold 12px Arial'}).setOrigin(0.5,0.5)
+       this.character = this.add.sprite(-100,600,"run") 
+       this.character.setScale(0.8,0.8)
+       this.character.anims.play('run',true)
+       this.character.flipX = true
+       this.shotgun = this.add.image(this.character.x+30, this.character.y+20, "shotgun");
+       this.shotgun.setScale(0.8,0.8)
         //Buttons creation
         var play_button = new Button({
             'scene': this,
@@ -75,6 +80,9 @@ class MainMenu extends Phaser.Scene {
         //Nubes
         this.cloud.x++;
         this.cloud1.x--;
+        this.character.x++;
+        this.shotgun.x++;
+      
         //this.cloud.y++;
         if(this.cloud.x>1500){
             this.cloud.x=200;
@@ -82,6 +90,11 @@ class MainMenu extends Phaser.Scene {
         if(this.cloud1.x<-200){
             this.cloud1.x=1500;
         }
+        if(this.character.x> 1400){
+            this.character.x = -100
+            this.shotgun.x = this.character.x+30
+        }
+        
         /*
         if(Math.sin(time)>-0.5){
 
