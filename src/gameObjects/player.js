fb88f6f593e2b63cc9ea70803e9a5a632396e8ab
Javinigadela <90959761+Javinigadela@users.noreply.github.com>
2021-10-05 19:07:00 +0200

    Cambio en jugador
    Se han eliminado funcionalidades y variables que ya no se necesitaban y se ha modificado ligeramente las funciones de incremento y decremento de balas y vidas.

--- src/gameObjects/player.js
+++ src/gameObjects/player.js
@@ -1,4 +1,3 @@
-import Bullet from './bullet.js';
 class Player extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, type){
         super(scene, x, y, type);
@@ -17,19 +16,10 @@ class Player extends Phaser.GameObjects.Sprite{
         this.weapon = scene.add.image(this.x, this.y+2, "shotgun");
         this.weapon.setOrigin(0.1, 0)
         this.weapon.setScale(0.5,0.5)
-        // example bullet
-        this.bullets = [];
-
-        // Holds the scene reference to pass it to the bullet constructor
-        this.scene = scene;
     }
     
 };
 
-Player.prototype.shootAt = function(xPlayer, yPlayer, xTarget, yTarget){
-    this.bullet = new Bullet(this.scene, xPlayer, yPlayer, "bullet", xTarget, yTarget, this.getAmmoSpeed());
-    return this.bullet;
-}
 Player.prototype.aim = function(xTarget, yTarget){
     this.weapon.rotation = Math.atan((yTarget-this.weapon.y) / (xTarget-this.weapon.x));
 }
@@ -39,8 +29,8 @@ Player.prototype.aim = function(xTarget, yTarget){
 Player.prototype.setLives = function(lives){this.lives = lives; };
 Player.prototype.getLives = function(){return this.lives; };
 
-Player.prototype.increaseLivesByOne = function(){this.lives -= 1; };
-Player.prototype.decreaseLivesByOne = function(){this.lives += 1; };
+Player.prototype.increaseLivesByOne = function(){this.lives += 1; };
+Player.prototype.decreaseLivesByOne = function(){this.lives -= 1; };
 
 Player.prototype.setAmmo = function(ammo){this.ammo = ammo; };
 Player.prototype.getAmmo = function(){return this.ammo; };
@@ -48,8 +38,8 @@ Player.prototype.getAmmo = function(){return this.ammo; };
 Player.prototype.setAmmoSpeed = function(speed){this.ammoSpeed = speed; };
 Player.prototype.getAmmoSpeed = function(){return this.ammoSpeed; };
 
-Player.prototype.increaseAmmoByOne = function(){this.ammo -= 1; };
-Player.prototype.decreaseAmmoByOne = function(){this.ammo += 1; };
+Player.prototype.increaseAmmoByOne = function(){this.ammo += 1; };
+Player.prototype.decreaseAmmoByOne = function(){this.ammo -= 1; };
 
 Player.prototype.isAlive = function(){return this.alive; };
 
