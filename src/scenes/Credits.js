740c8aa402b64558cbe79f785b209d0b6d4ba9b0
Javinigadela <90959761+Javinigadela@users.noreply.github.com>
2021-10-24 12:50:20 +0200

    nuevo fondo y animacion
    Se añade la imagen de fondo, el nuevo botón y su animacion

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -6,19 +6,31 @@ class Credits extends Phaser.Scene {
 
 create(){
     this.cameras.main.fadeIn(250, 0, 0, 0)
-    this.text=this.add.text(640,80, "CREDITS",{font: 'bold 32px Arial', fontSize: "36px"}).setOrigin(0.5,0.5)
+    this.text=this.add.text(640,80, "CREDITS",{font: 'bold 32px Arial', fontSize: "36px"}).setOrigin(0.5,0.5);
+    this.add.image(640, 320, "creditsBackground").setScale(0.4,0.4);
 
     this.add.image(640, 308, "background").setScale(0.42,0.42);
     var back_button = new Button({
         'scene': this,
-        'key':'setting_button',
+        'key':'back_buttons',
         'up': 0,
         'over':1,
         'down':2,
-        'x': 300,
-        'y': 180
+        'x': 1030,
+        'y': 200
     }).setScale(0.7,0.7);
     back_button.on('pointerup',this.goBack,this)
+    
+    // Button "animation"
+    this.input.on('pointerdown', function (pointer) {
+        //image back_button: width:236, height: 95. Scale: 0.7 both axes
+        if((pointer.x > 1030-(236/2)*0.7 && pointer.x < 1030+(236/2)*0.7 && pointer.y > 200-(95/2)*0.7 && pointer.y < 200+(95/2)*0.7)) //back button
+        {
+            var bulletMenuSound = this.sound.add('shot');
+            bulletMenuSound.play({volume: this.registry.get('effectsVolumeFromMenu')});
+            this.trigger = this.add.image(pointer.x, pointer.y, "bulletMenu").setScale(0.3);
+        }
+        }, this);
 }
 goBack(){
     this.scene.stop("Credits")
