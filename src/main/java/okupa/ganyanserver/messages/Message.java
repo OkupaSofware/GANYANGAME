dea43a9ebc05feebb9ddd5ffa049c6db455d0be7
aantonb2018 <alexantber@gmail.com>
2021-11-16 23:39:17 +0100

    [messages] Creacion Message.java
    Clase que actuará como el objeto de los mensajes. Cuenta con una fecha de creación, un nombre de autor y el texto del mensaje

---
+++ src/main/java/okupa/ganyanserver/messages/Message.java
@@ -0,0 +1,42 @@
+package com.example.demo.messages;
+
+public class Message {
+
+	String date; //La fecha/hora en la que se creo el mensaje
+	String username; //El autor del mensaje
+	String textMessage; //El autor del mensaje
+	
+	//Constructores
+	public Message() {}
+	
+	public Message(String date, String username, String textMessage) {
+		this.date = date;
+		this.username = username;
+		this.textMessage = textMessage;
+	}
+	
+	//Getters y setters
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getTextMessage() {
+		return textMessage;
+	}
+	
+	public void setTextMessage(String textMessage) {
+		this.textMessage = textMessage;
+	}
+}
